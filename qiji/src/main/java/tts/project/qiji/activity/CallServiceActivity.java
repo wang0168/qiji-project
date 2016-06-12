8c29fc34e288d0f7ffaae3e1ba57261953a7b991
shanghang88 <490956364@qq.com>
2016-06-12 17:29:30 +0800

    添加页面

--- qiji/src/main/java/tts/project/qiji/activity/CallServiceActivity.java
+++ qiji/src/main/java/tts/project/qiji/activity/CallServiceActivity.java
@@ -2,13 +2,17 @@ package tts.project.qiji.activity;
 
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import tts.moudle.api.widget.RecyclerViewAutoRefreshUpgraded;
 import tts.project.qiji.BaseActivity;
 import tts.project.qiji.R;
+import tts.project.qiji.adapter.CallServiceAdapter;
 
 public class CallServiceActivity extends BaseActivity {
     private RecyclerViewAutoRefreshUpgraded mlist;
+    private View header;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +24,14 @@ public class CallServiceActivity extends BaseActivity {
 
     private void adapter() {
         mlist.setLayoutManager(new GridLayoutManager(this, 3));
+        CallServiceAdapter callServiceAdapter = new CallServiceAdapter(this, null);
+        mlist.setAdapter(callServiceAdapter);
+
     }
 
     private void findAllView() {
+        header = LayoutInflater.from(this).inflate(R.layout.layout_call_service_header, null, false);
         mlist = (RecyclerViewAutoRefreshUpgraded) findViewById(R.id.mlist);
-
+        mlist.addHeader(header);
     }
 }
