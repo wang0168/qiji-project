c3e96fe13ad90d0d08223288d6b6bce96b1efe37
wangqili <wangqili0159@163.com>
2016-06-15 10:46:05 +0800

    aa

--- qiji/src/main/java/tts/project/qiji/bean/CollectionBean.java
+++
@@ -1,7 +0,0 @@
-package tts.project.qiji.bean;
-
-/**
- * Created by shanghang on 2016/6/12.
- */
-public class CollectionBean {
-}
--- qiji/src/main/java/tts/project/qiji/bean/UserInfoBean.java
+++
@@ -1,7 +0,0 @@
-package tts.project.qiji.bean;
-
-/**
- * Created by shanghang on 2016/6/7.
- */
-public class UserInfoBean {
-}
